1439512b5efebf4f3f38a56af6f8f5f630f4723d
JOAO DIVINO DA SILVA <60980853+joaodivinod@users.noreply.github.com>
2020-11-14 14:43:09 -0300

    WIP

---
+++ src/components/task.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="task" :class="stateClass">
+<p>{{task.name}}</p>
+</div>
+</template>
+
+<script>
+export default {
+name: "task",
+  props:{
+    task:{type:Object,require: true}
+  },
+  computed:{
+    stateClass(){
+      return{
+        pending :this.task.pending,
+        done:!this.task.pending,
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .task{
+    box-sizing: border-box;
+    width: 350px;
+    height: 150px;
+    padding: 10px;
+    border-radius: 2rem;
+    font-weight: 300;
+    cursor: pointer;
+    user-select: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .pending{
+    border-left:12px solid #b73229 ;
+    background-color: #b73229;
+  }
+  .done{
+    color: #ddd;
+    border-left: 12px solid  #0a8f08;
+    background-color:#0a8f08;
+    text-decoration: line-through;
+  }
+</style>
--- src/components/taskGrid.vue
+++ src/components/taskGrid.vue
@@ -1,18 +1,20 @@
 <template>
 
-  <div class="tasks">
+  <div class="tasks-grid">
    <template>
-     <div  v-for="task in tasks" :key="task.name">
-       {{task.name}}
-     </div>
+     <task  v-for="task in tasks" :key="task.name" :task="task" >
+
+     </task>
    </template>
   </div>
 
 </template>
 
 <script>
+import task from "@/components/task.vue";
 export default {
   name: "taskGrid",
+  components:{task},
   props: {
     tasks:{type:Array, requirement:true}
   }
